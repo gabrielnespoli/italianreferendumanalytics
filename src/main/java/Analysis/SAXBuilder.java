f71274ce1bb4e50d1027446caa7ac26e227ff9fb
gabriel <gabriel@gabriel-Aspire-E5-575G>
2018-03-12 20:37:02 +0100

    formatted the spaces, tabs, for's, if's, etc. from the  temporal analysis, changed the output to a txt file with the topN terms and the cluster each term belongs to

--- src/main/java/Analysis/SAXBuilder.java
+++ src/main/java/Analysis/SAXBuilder.java
@@ -4,19 +4,21 @@ import net.seninp.jmotif.sax.SAXException;
 import net.seninp.jmotif.sax.SAXProcessor;
 import net.seninp.jmotif.sax.alphabet.Alphabet;
 import net.seninp.jmotif.sax.datastructure.SAXRecords;
+
 public class SAXBuilder {
+
     private final int alphabetSize;
     private final double nThreshold;
     private final Alphabet na;
-    
-    public SAXBuilder(int alphabetSize, double nThreshold, Alphabet na){
+
+    public SAXBuilder(int alphabetSize, double nThreshold, Alphabet na) {
         this.alphabetSize = alphabetSize;
         this.nThreshold = nThreshold;
         this.na = na;
     }
-    
+
     // from a timeseries of the word frequencies, create a SAX string that represents it
-    public String buildSAX(double[] timeSeries) throws SAXException{
+    public String buildSAX(double[] timeSeries) throws SAXException {
         SAXProcessor sp = new SAXProcessor();
         SAXRecords res = sp.ts2saxByChunking(timeSeries, timeSeries.length, na.getCuts(alphabetSize), nThreshold);
         return res.getSAXString("");
