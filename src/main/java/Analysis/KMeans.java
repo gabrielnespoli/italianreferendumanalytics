a3f8463c7afabfd8dd2bc52f4ed529fd6331e9eb
gabriel <gabriel@gabriel-Aspire-E5-575G>
2018-03-16 20:45:15 +0100

    -corrected the problem in kmeans that was now looping till the convergence -changed the way kmeans was calculating the centroid, using the ASCII value of the chars, instead of using hamming distance to calculate distance -implemented the class CharUtils, which offers two methods, one to calculate an average char between an array of chars and another to calculate a distance between two chars, using their difference in the ASCII table

--- src/main/java/Analysis/KMeans.java
+++ src/main/java/Analysis/KMeans.java
@@ -1,23 +1,30 @@
 package Analysis;
 
+import Utils.CharUtils;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashSet;
 import org.apache.commons.text.CharacterPredicates;
 import org.apache.commons.text.RandomStringGenerator;
-import org.apache.commons.text.similarity.HammingDistance;
 
 public class KMeans {
 
-    private final HammingDistance distanceCalculator;
+    private final HashMap<String, Integer> hmSaxCluster;
+    private final int k;
+    private final int alphabetSize;
+    private final ArrayList<String> saxStrings;
 
-    public KMeans(HammingDistance distanceCalculator) {
-        this.distanceCalculator = distanceCalculator;
+    public KMeans(int k, int alphabetSize, ArrayList<String> saxStrings) {
+        this.hmSaxCluster = new HashMap<>();
+        this.k = k;
+        this.alphabetSize = alphabetSize;
+        this.saxStrings = saxStrings;
     }
 
-    // get a random centroid in the space vector of SAX strings
-    // the centroid will have the length of saxStringSize and composed by characters from 'a' to lastAlphabetLetter
-    private String getCentroid(char maxAlphabetLetter, int saxStringSize) {
+    private String generateRandomCentroids() {
+        char maxAlphabetLetter = (char) (97 + this.alphabetSize); //97 is the 'a' value in ASCII
+        int saxStringSize = this.saxStrings.get(0).length();
+
         RandomStringGenerator randomStringGenerator
                 = new RandomStringGenerator.Builder()
                         .withinRange('a', maxAlphabetLetter)
@@ -26,61 +33,132 @@ public class KMeans {
         return randomStringGenerator.generate(saxStringSize);
     }
 
-    // return an array of the same size of items which in each position is stored the cluster of that item
-    public int[] getClusterIDs(int k, HashMap<String, String> hmSaxStrings) {
-        ArrayList<String> listItems = new ArrayList<>(hmSaxStrings.values()); //"convert" the collection to arraylist
-        int listSize = listItems.size();
-        int[] arrayClusterID = new int[listSize];
-
-        // initialize the centroids
-        String[] centroids = new String[k];
-        for (int i = 0; i < k; i++) {
-            centroids[i] = getCentroid((char) listItems.get(0).chars().max().getAsInt(), listItems.get(0).length());
+    private String[] getRandomCentroids() {
+        String[] centroids = new String[this.k];
+        for (int i = 0; i < this.k; i++) {
+            centroids[i] = generateRandomCentroids();
         }
 
-        // iterate through all items
-        for (int i = 0; i < listSize; i++) {
-            Integer minDistance = Integer.MAX_VALUE;
-            String item = listItems.get(i);
-            // iterate through all centroids
-            int newDist;
-            for (int j = 0; j < k; j++) {
-                newDist = distanceCalculator.apply(item, centroids[j]);
-                if (newDist < minDistance) {
-                    minDistance = newDist;
-                    arrayClusterID[i] = j;
-                }
+        return centroids;
+    }
+
+    private String getCentroid(LinkedHashSet<String> cluster) {
+        ArrayList<String> clusterList = new ArrayList<>(cluster);  // convert to list to allow indexed
+        String centroid = "";
+
+        // eventually after the first iteration, maybe there isn't any item in a
+        // specific cluster (because a random generated centroid could have been 
+        // bad localized). In this case, there isn't any update in the baricenter
+        if (cluster.isEmpty()) {
+            return generateRandomCentroids();
+        }
+
+        int saxStringSize = 0;
+        for (String saxString : cluster) {
+            saxStringSize = saxString.length();
+            break;
+        }
+
+        // iterate through all the SAX strings of the cluster, calculating the average
+        // char value for each position of the strings at a time.
+        for (int i = 0; i < saxStringSize; i++) {
+            char[] chars = new char[cluster.size()];  // chars carry all the i-th characters being read, that will be averaged
+            for (int j = 0; j < clusterList.size(); j++) {
+                chars[j] = clusterList.get(j).charAt(i);
+            }
+
+            centroid += CharUtils.calculateAverageChar(chars);
+        }
+
+        return centroid;
+    }
+
+    /* Calculate the centroid "position" by the datapoints of the cluster i
+    The centroid will have the length of saxStringSize and composed by characters from 'a' to lastAlphabetLetter
+     */
+    private String[] getCentroids(ArrayList<LinkedHashSet<String>> clusters) {
+        String[] centroids = new String[this.k];
+
+        for (int i = 0; i < this.k; i++) {
+            centroids[i] = getCentroid(clusters.get(i));
+        }
+
+        return centroids;
+    }
+
+    /*
+    receive a list of clusters, the saxString to be added and the clusterID that
+    the saxString need to be added. It updates the auxiliary structure 'clusters' 
+    and the structure hmSaxCluster, that contains all the saxStrings and the
+    cluster they belong to.
+     */
+    private void updateCluster(ArrayList<LinkedHashSet<String>> clusters, String saxString, int newClusterID) {
+        // check if the sax string was already put in another cluster. If so, remove it
+        if (this.hmSaxCluster.containsKey(saxString)) {
+            int oldClusterID = this.hmSaxCluster.get(saxString);
+            if (oldClusterID != newClusterID) {
+                LinkedHashSet<String> oldCluster = clusters.get(oldClusterID);
+                oldCluster.remove(saxString);
             }
         }
-        return arrayClusterID;
+
+        // add saxString to the new cluster
+        LinkedHashSet<String> cluster = clusters.get(newClusterID);  // load the cluster 'newClusterID'
+        cluster.add(saxString);  // add the sax string in position i
+        this.hmSaxCluster.put(saxString, newClusterID); //update the cluster ID of the sax string in the map
     }
 
-    // returns a list of LinkedHashSet, in which in each position of the list is 
-    // a cluster (LinkedHashSet) of the similar terms (strings)
-    public ArrayList<LinkedHashSet<String>> getClusterOfTerms(int k, HashMap<String, String> hmSaxStrings) {
-        int[] arrayClusterID = this.getClusterIDs(k, hmSaxStrings);
+    /*
+    return an array of the same size of items which in each position is stored 
+    the cluster of that item
+     */
+    public HashMap<String, Integer> getClusters() throws Exception {
+        int listSize = saxStrings.size();
+        int totalErrorOld = 0;
+        int totalErrorNew = 0;  // assigned any value just to enter in the foor loop
+        String[] centroids;
 
-        ArrayList<String> saxStrings = new ArrayList<>(hmSaxStrings.values()); //"convert" the collection of saxstrings to arraylist
-
-        // create a list with the terms/words to allow indexed access
-        ArrayList<String> terms = new ArrayList<>();
-        hmSaxStrings.keySet().forEach((term) -> {
-            terms.add(term);
-        });
-
-        //instantiate the sets inside the list
-        ArrayList<LinkedHashSet<String>> clusterOfTerms = new ArrayList<>();
+        // clusters is an auxiliary structure that allows indexed access
+        // instantiate the empty clusters (sets) inside the list of clusters
+        ArrayList<LinkedHashSet<String>> clusters = new ArrayList<>();
         for (int i = 0; i < k; i++) {
-            clusterOfTerms.add(new LinkedHashSet<>());
+            clusters.add(new LinkedHashSet<>());
         }
 
-        //iterate through the cluster IDs adding creating the set of similar terms of each cluster ID
-        for (int i = 0; i < arrayClusterID.length; i++) {
-            int clusterID = arrayClusterID[i];                              //get the cluster ID in the position i
-            LinkedHashSet<String> cluster = clusterOfTerms.get(clusterID);  //get the set of terms of clusterID
-            cluster.add(terms.get(i));                                      //and add the term in position i
-        }
+        centroids = getRandomCentroids();
 
-        return clusterOfTerms;
+        do {
+            totalErrorOld = totalErrorNew;
+            totalErrorNew = 0;
+
+            // iterate through all sax strings comparing the distance to the centroids
+            for (int i = 0; i < listSize; i++) {
+                Integer minDistance = Integer.MAX_VALUE;
+                String saxString = this.saxStrings.get(i);
+
+                // iterate through all centroids
+                int newDist;
+                int newClusterID = 0;
+                for (int clusterID = 0; clusterID < k; clusterID++) {
+                    newDist = CharUtils.calculateDistance(saxString.toCharArray(), centroids[clusterID].toCharArray());
+                    //newDist = distanceCalculator.apply(saxString, centroids[clusterID]);
+
+                    // the sax string will be added to the cluster that has the mininum distance
+                    // from its centroid to the sax String
+                    if (newDist < minDistance) {
+                        minDistance = newDist;
+                        newClusterID = clusterID;
+                    }
+                }
+
+                totalErrorNew += minDistance; // update the total error
+                updateCluster(clusters, saxString, newClusterID);
+            }
+
+            centroids = getCentroids(clusters); // calculate the centroids for the next iteration
+        } while (totalErrorOld != totalErrorNew);
+
+        return hmSaxCluster;
     }
+
 }
---
+++ src/main/java/Utils/CharUtils.java
@@ -0,0 +1,54 @@
+package Utils;
+
+import java.util.HashMap;
+
+/* 
+Class that provide a method to calculate the average char in a sequence of chars. 
+Ex: calculateAverageChar([a,c]) = b
+Provides also a static method to calculate distance from 2 vector of chars
+ */
+public abstract class CharUtils {
+
+    private static final HashMap<Character, Integer> hmCharInt;
+    private static final HashMap<Integer, Character> hmIntChar;
+
+    static {
+        hmCharInt = new HashMap<>();
+        hmIntChar = new HashMap<>();
+
+        // iterate through all the chars (from a to z)
+        for (int c = 97, value = 0; c < 123; c++, value++) {
+            hmCharInt.put((char) c, value);
+            hmIntChar.put(value, (char) c);
+        }
+    }
+
+    public static char calculateAverageChar(char[] chars) {
+        int average = 0;
+        for (int i = 0; i < chars.length; i++) {
+            average += hmCharInt.get(chars[i]);
+        }
+        average = (int) Math.round(average / chars.length);
+
+        return hmIntChar.get(average);
+    }
+
+    /*
+    Calculate the distance between two vector of chars. The total distance is a summation
+    of the distance of each char in the vectors. A distance from two chars is their
+    difference in ASCII value
+    */
+    public static int calculateDistance(char[] chars1, char[] chars2) throws Exception {
+        if (chars1.length != chars2.length) {
+            throw new Exception("Vector of chars with different size");
+        }
+
+        int totalDistance = 0;
+        for (int i = 0; i < chars1.length; i++) {
+            totalDistance += Math.abs(chars1[i] - chars2[i]);
+        }
+
+        return totalDistance;
+    }
+
+}
